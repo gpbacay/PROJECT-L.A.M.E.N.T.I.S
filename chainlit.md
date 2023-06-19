e5bbb6da6caf7fd83a38328ba5474399a56c4343
gpbacay <111699545+gpbacay@users.noreply.github.com>
2023-06-20 01:02:16 +0800

    lamentis1 update

---
+++ chainlit.md
@@ -0,0 +1,6 @@
+# L.A.M.E.N.T.I.S 🌚
+## (Language-based Artificial-Intelligence Model with Enhanced Natural-language-processing and Text-based Intelligent System)
+
+Hi, I am LAMENTIS, short term for Language-based Artificial-Intelligence Model with Enhanced Natural-language-processing and Text-based Intelligent System. I am an Artificial Intelligence Personal Virtual Assistant created by Gianne P. Bacay. 
+
+What can I assist you with?
--- lamentis.py
+++ lamentis.py
@@ -5,75 +5,57 @@ print(header)
 #huggingface_api_key = "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"
 
 import os
-from dotenv import load_dotenv, find_dotenv
-from langchain import HuggingFaceHub
-from langchain import PromptTemplate, LLMChain, OpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.summarize import load_summarize_chain
-from langchain.document_loaders import YoutubeLoader
 import textwrap
 import pyttsx3
+from dotenv import load_dotenv, find_dotenv
+from langchain import HuggingFaceHub
+from langchain import PromptTemplate, LLMChain
+import sys
 
+def run_lamentis():
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
 
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+    def speak(text):
+        engine.say(text)
+        engine.runAndWait()
 
+    print("Initializing...")
+    speak("Initializing LAMENTIS")
 
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
-    
-print("Initializing...")
-speak("Initializing LAMENTIS")
+    # Load the HuggingFaceHub API token from the .env file
+    load_dotenv(find_dotenv())
+    huggingfacehub_api_token = os.environ["HUGGINGFACEHUB_API_TOKEN"]
 
-# --------------------------------------------------------------
-# Load the HuggingFaceHub API token from the .env file
-# --------------------------------------------------------------
+    # Load the LLM model from the HuggingFaceHub
+    repo_id = "tiiuae/falcon-7b-instruct"
+    falcon_llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.1, "max_new_tokens": 500})
 
-load_dotenv(find_dotenv())
-HUGGINGFACEHUB_API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"]
+    # Create a PromptTemplate and LLMChain
+    template = """Your name is LAMENTIS, short for Language-based Artificial-Intelligence Model with Enhanced Natural-language-processing and Text-based Intelligent System. 
+    You are an Artificial Intelligence Personal Virtual Assistant created by Gianne P. Bacay.
 
+    Question: {question}"""
 
-# --------------------------------------------------------------
-# Load the LLM model from the HuggingFaceHub
-# --------------------------------------------------------------
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm_chain = LLMChain(prompt=prompt, llm=falcon_llm)
 
-repo_id = "tiiuae/falcon-7b-instruct"
-falcon_llm = HuggingFaceHub(
-    repo_id=repo_id, model_kwargs={"temperature": 0.1, "max_new_tokens": 500}
-)
-
-
-# --------------------------------------------------------------
-# Create a PromptTemplate and LLMChain
-# --------------------------------------------------------------
-template = """Your name is LAMENTIS, short for Language-based Artificial-Intelligence Model with Enhanced Natural-language-processing and Text-based Intelligent System. 
-You are an Artificial Intelligence Personal Virtual Assistant created by Gianne P. Bacay.
-
-Question: {question}
-
-Answer:"""
-
-prompt = PromptTemplate(template=template, input_variables=["question"])
-llm_chain = LLMChain(prompt=prompt, llm=falcon_llm)
-
-
-# --------------------------------------------------------------
-# Run the LLMChain
-# --------------------------------------------------------------
-if __name__ == '__main__':
+    # Run the LLMChain
     response = "System is now online."
     print(response)
     speak(response)
     while True:
         question = input("\ninput: ")
+        if "quit" in question:
+            break
         response = llm_chain.run(question)
-        wrapped_text = textwrap.fill(response, width=100, break_long_words=False, replace_whitespace=False)
-        print("output: " + wrapped_text)
+        wrapped_text = textwrap.fill(response, width=200, break_long_words=False, replace_whitespace=False)
+        print("LAMENTIS: " + wrapped_text)
         speak(response)
 
-
+if __name__ == '__main__':
+    run_lamentis()
 
 
 #__________________________python lamentis.py
